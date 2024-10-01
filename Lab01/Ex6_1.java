c3dc6d7c3ef1d5e9d513325d95173593cdb33874
MinhNguyen2715 <143490448+MinhNguyen2715@users.noreply.github.com>
2024-10-01 13:38:55 +0700

    adding Lab01

---
+++ Lab01/Ex6_1.java
@@ -0,0 +1,26 @@
+package Lab1;
+
+import javax.swing.JOptionPane;
+public class Ex6_1 {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null,
+                "Do you want to change to the first class ticket?");
+
+//        Customize button
+//
+//        String[] options = {"I do",
+//                "I don't"};
+//        int option = JOptionPane.showOptionDialog(null,
+//                "Do you want to change to the first class ticket?",
+//                "Confirmation",
+//                JOptionPane.YES_NO_OPTION,
+//                JOptionPane.QUESTION_MESSAGE,
+//                null,     //do not use a custom Icon
+//                options,  //the titles of buttons
+//                options[0]); //default button title
+
+        JOptionPane.showMessageDialog(null,"You've chosen: "+ (option==JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+
+    }
+}
