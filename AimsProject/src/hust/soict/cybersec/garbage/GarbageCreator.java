a364bbfdecaa7d30d58c2b3de04097a8176d4ea4
MinhNguyen2715 <143490448+MinhNguyen2715@users.noreply.github.com>
2024-11-23 22:24:42 +0700

    String, StringBuffer, StringBuilder exercise

---
+++ AimsProject/src/hust/soict/cybersec/garbage/GarbageCreator.java
@@ -0,0 +1,50 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class GarbageCreator {
+    public static void main(String[] args) throws IOException {
+        String filePath = "AimsProject/src/hust/soict/cybersec/garbage/text.txt";
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // Generate 100000 random words
+        for (int i = 0; i < 100000; i++) {
+            int wordLength = random.nextInt(8) + 3;
+            StringBuilder word = new StringBuilder();
+
+            for (int j = 0; j < wordLength; j++) {
+                char letter = (char) ('a' + random.nextInt(26));
+                word.append(letter);
+            }
+
+            stringBuilder.append(word).append(" ");
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(stringBuilder.toString());
+            System.out.println("File generated successfully at: " + filePath);
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+
+        //Read String without StringBuffer
+
+        String filename = "AimsProject/src/hust/soict/cybersec/garbage/text.txt";
+        byte[] inputBytes = {0};
+        long startTime, endTime;
+
+        inputBytes = Files.readAllBytes(Paths.get(filename));
+        startTime = System.currentTimeMillis();
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char)b;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
+
+    }
+}
